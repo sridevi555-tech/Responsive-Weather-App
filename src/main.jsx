c0e5a0562c39958adf90e6973cf139a056ca8e03
Peddireddy Sridevi <142387634+sridevi555-tech@users.noreply.github.com>
2023-11-04 12:02:27 +0530

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './index.css'
+import WeatherApp from './Components/WeatherApp/WeatherApp'
+
+
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+   <WeatherApp/>
+  </React.StrictMode>,
+)
